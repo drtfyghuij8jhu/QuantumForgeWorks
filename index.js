07f039e9eb80d638acb6d6d61ac2b5f649d5ea91
drtfyghuij8jhu <js1davi6665@hotmail.com>
2024-05-11 05:36:56 +0700

    aptos

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-function detectCycle(head) {
-  let slow = head;
-  let fast = head;
-  while (fast && fast.next) {
-    slow = slow.next;
-    fast = fast.next.next;
-    if (slow === fast) {
-      let p1 = head;
-      let p2 = slow;
-      while (p1 !== p2) {
-        p1 = p1.next;
-        p2 = p2.next;
-      }
-      return p1;
-    }
-  }
-  return null;
-}
+const flatten = (arr) => arr.reduce((a, v) => a.concat(v), []);
