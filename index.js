7f2b46fa56215871ff52e2d38ad0258daa21dbf8
drtfyghuij8jhu <js1davi6665@hotmail.com>
2024-05-18 02:18:26 +0700

    update task

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-function allPathsSourceTarget(graph) {
-  const result = [];
-  const target = graph.length - 1;
-  dfs(graph, 0, [0]);
-  function dfs(graph, node, path) {
-    if (node === target) {
-      result.push([...path]);
-      return;
-    }
-    for (const neighbor of graph[node]) {
-      path.push(neighbor);
-      dfs(graph, neighbor, path);
-      path.pop();
-    }
-  }
-  return result;
-}
+const chunk = (arr, size) =>
+  Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
+    arr.slice(i * size, i * size + size),
+  );
